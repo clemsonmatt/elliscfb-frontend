785a28190f12ea26bea1b6119f38f70c90b61b84
Matt Ellis <mellis0292@gmail.com>
2023-07-26 09:57:42 -0400

    Check for admin permissions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import axios from 'axios'
+import { useAuthStore } from '@/stores/auth.store'
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
-// Add a request interceptor
+// Add a request interceptor for auth token
 axios.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token')
@@ -17,6 +18,21 @@ axios.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
+// redirect to login page if needed
+axios.interceptors.response.use(undefined, function (error) {
+  const authStore = useAuthStore()
+
+  if (error) {
+    const originalRequest = error.config
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      // store.dispatch('LogOut')
+      authStore.logout()
+      return router.push({ name: 'cfb_login' })
+    }
+  }
+})
 
 import App from './App.vue'
 import router from './router'
