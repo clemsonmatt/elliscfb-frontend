4d17d2d59e414f590123deb10bfa417987936375
Matt Ellis <mellis0292@gmail.com>
2023-08-28 16:08:20 -0400

    Locked vs unlocked components

--- src/views/PickemView.vue
+++ src/views/PickemView.vue
@@ -2,6 +2,8 @@
 import BaseLayout from '@/views/BaseLayout.vue'
 import AlertComponent from '@/components/Alert.vue'
 import SpinnerComponent from '@/components/Spinner.vue'
+import PickemComponent from '@/components/Pickem.vue'
+import PickemLockedComponent from '@/components/PickemLocked.vue'
 </script>
 
 <template>
@@ -35,127 +37,22 @@ import SpinnerComponent from '@/components/Spinner.vue'
             <AlertComponent color="error" :message="`${error}`" />
           </div>
           <div v-for="game in games">
-            <div class="mt-6 card">
-              <div class="card-body">
-                <div class="grid items-center grid-cols-3 gap-4 justify-items-stretch">
-                  <div class="card-link" @click="pickWinner(game, game.away_team)">
-                    <div class="items-center text-center card-body">
-                      <img :src="`../teamLogos/${game.away_team.logo}`" class="w-24" />
-                      <span v-if="game.away_team.slug == game.predicted_winning_team.slug">
-                        {{ game.away_team.name_short }} (-{{ game.spread }})
-                      </span>
-                      <span v-else>{{ game.away_team.name_short }}</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-10 h-10 mt-2 rounded-full"
-                        :class="{
-                          'text-white bg-primary': picks.includes(game.away_team.slug),
-                          ' text-base-100 bg-base-300': !picks.includes(game.away_team.slug)
-                        }"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div class="text-center">
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-10 h-10 mx-auto mb-4 text-primary"
-                        v-if="!canPick(game)"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                        />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-10 h-10 mx-auto mb-4 text-primary"
-                        v-if="canPick(game)"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M13.5 10.5V6.75a4.5 4.5 0 119 0v3.75M3.75 21.75h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H3.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                        />
-                      </svg>
-                      {{ game.location }}
-                      <br />
-                      {{ game.home_team.city }}, {{ game.home_team.state }}
-                    </div>
-                    <div class="w-full my-5">
-                      <div class="divider">
-                        <router-link
-                          v-bind:to="{ name: 'cfb_game', params: { id: game.id.toString() } }"
-                          class="btn btn-sm btn-primary"
-                        >
-                          Game details
-                        </router-link>
-                      </div>
-                    </div>
-                    <div>
-                      {{
-                        new Date(game.datetime).toLocaleDateString('en-us', {
-                          month: 'short',
-                          day: '2-digit',
-                          weekday: 'long'
-                        })
-                      }}
-                      <br />
-                      {{ game.time }}
-                      <div class="ml-2 badge badge-default badge-outline" v-if="game.network">
-                        {{ game.network }}
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card-link" @click="pickWinner(game, game.home_team)">
-                    <div class="items-center text-center card-body">
-                      <img :src="`../teamLogos/${game.home_team.logo}`" class="w-24" />
-                      <span v-if="game.home_team.slug == game.predicted_winning_team.slug">
-                        {{ game.home_team.name_short }} (-{{ game.spread }})
-                      </span>
-                      <span v-else>{{ game.home_team.name_short }}</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-10 h-10 mt-2 rounded-full"
-                        :class="{
-                          'text-white bg-primary': picks.includes(game.home_team.slug),
-                          ' text-base-100 bg-base-300': !picks.includes(game.home_team.slug)
-                        }"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PickemComponent
+              :game="game"
+              :away-team-picked="picks.includes(game.away_team.slug)"
+              :home-team-picked="picks.includes(game.home_team.slug)"
+              @home-team-picked="pickWinner(game, game.home_team)"
+              @away-team-picked="pickWinner(game, game.away_team)"
+              v-if="canPick(game)"
+            />
+            <PickemLockedComponent
+              :game="game"
+              :away-team-picked="picks.includes(game.away_team.slug)"
+              :home-team-picked="picks.includes(game.home_team.slug)"
+              @home-team-picked="pickWinner(game, game.home_team)"
+              @away-team-picked="pickWinner(game, game.away_team)"
+              v-if="!canPick(game) && game.winning_team == null"
+            />
           </div>
         </div>
       </div>
