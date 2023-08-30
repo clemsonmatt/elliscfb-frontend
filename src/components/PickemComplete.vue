cf5b6afc2b3664a32efe55be5f44c6abb52b71c2
Matt Ellis <mellis0292@gmail.com>
2023-08-30 14:49:36 -0400

    Show mobile pickem winning picks

--- src/components/PickemComplete.vue
+++ src/components/PickemComplete.vue
@@ -23,20 +23,11 @@ const props = defineProps<{
             }}
           </div>
           <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-10 h-10 mx-auto text-primary"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-              />
-            </svg>
+            <h3 class="text-2xl text-primary">
+              {{ props.game.home_team_stats.final }}
+              &mdash;
+              {{ props.game.away_team_stats.final }}
+            </h3>
           </div>
           <div>
             {{ props.game.time }}
