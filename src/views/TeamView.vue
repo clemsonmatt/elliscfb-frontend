987dec3b28c403aacaeaddd1e1b7b525aafbe4b6
Matt Ellis <mellis0292@gmail.com>
2023-08-30 10:33:08 -0400

    Mobile improvements for team details

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -4,62 +4,60 @@ import TeamNavbar from '@/components/TeamNavbar.vue'
 import AlertComponent from '@/components/Alert.vue'
 import GameComponent from '@/components/Game.vue'
 import SpinnerComponent from '@/components/Spinner.vue'
-// import { watch } from 'vue'
-
-// let slug = this.$route.params.slug.toString()
-
-// watch(slug, (newSlug) => {
-
-// })
 </script>
 
 <template>
   <BaseLayout>
     <template #header>
-      <div class="flex items-center space-x-4">
+      <div class="flex items-center space-x-4" v-if="!teamLoading">
         <img :src="`../teamLogos/${team.logo}`" class="w-16" />
-        <span>{{ team.name }}</span>
+        <span class="hidden md:block">{{ team.name }}</span>
+        <span class="block md:hidden">{{ team.name_short }}</span>
       </div>
     </template>
 
     <template #default>
       <AlertComponent color="error" :message="error" v-if="error" />
 
-      <TeamNavbar :slug="team.slug" v-if="!teamLoading" />
-      <SpinnerComponent v-else></SpinnerComponent>
+      <SpinnerComponent v-if="teamLoading"></SpinnerComponent>
+      <div v-else>
+        <TeamNavbar :slug="team.slug" />
 
-      <div class="grid gap-4 xl:grid-cols-3">
-        <div class="row-span-2 card">
-          <h2 class="card-title">School Info</h2>
-          <div class="card-body">
-            <h4 class="text-xl italic font-bold underline">{{ team.school }}</h4>
-            <h5 class="font-bold">Location</h5>
-            <h6 class="italic">{{ team.city }}, {{ team.state }}</h6>
-            <h5 class="font-bold">Stadium</h5>
-            <h6 class="italic">{{ team.stadium_name }}</h6>
-            <h5 class="font-bold">Colors</h5>
-            <div class="grid grid-cols-3">
-              <div
-                class="h-6 col-span-2 rounded-l-xl"
-                :style="{ backgroundColor: `#${team.primary_color}` }"
-              ></div>
-              <div
-                class="h-6 rounded-r-xl"
-                :style="{ backgroundColor: `#${team.secondary_color}` }"
-              ></div>
-            </div>
-            <div class="grid grid-cols-3">
-              <h6 class="col-span-2 italic text-center">#{{ team.primary_color }}</h6>
-              <h6 class="italic text-center">#{{ team.secondary_color }}</h6>
+        <div class="grid gap-4 xl:grid-cols-3">
+          <div class="row-span-2 card">
+            <h2 class="card-title">School Info</h2>
+            <div class="card-body">
+              <h4 class="text-xl italic font-bold underline">{{ team.school }}</h4>
+              <h5 class="font-bold">Location</h5>
+              <h6 class="italic">{{ team.city }}, {{ team.state }}</h6>
+              <h5 class="font-bold">Stadium</h5>
+              <h6 class="italic">{{ team.stadium_name }}</h6>
+              <h5 class="font-bold">Colors</h5>
+              <div class="grid grid-cols-3">
+                <div
+                  class="h-6 col-span-2 rounded-l-xl"
+                  :style="{ backgroundColor: `#${team.primary_color}` }"
+                ></div>
+                <div
+                  class="h-6 rounded-r-xl"
+                  :style="{ backgroundColor: `#${team.secondary_color}` }"
+                ></div>
+              </div>
+              <div class="grid grid-cols-3">
+                <h6 class="col-span-2 italic text-center">#{{ team.primary_color }}</h6>
+                <h6 class="italic text-center">#{{ team.secondary_color }}</h6>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-span-2">
-          <div class="card">
-            <h2 class="card-title">Upcoming Game</h2>
+          <div class="md:col-span-2">
+            <div class="mt-6 mb-0 card">
+              <div class="card-body">
+                <h2 class="text-xl font-semibold uppercase">Next Game</h2>
+              </div>
+            </div>
+            <GameComponent :game="game" :isManage="false" v-if="!nextGameLoading" />
+            <SpinnerComponent v-else></SpinnerComponent>
           </div>
-          <GameComponent :game="game" :isManage="false" v-if="!nextGameLoading" />
-          <SpinnerComponent v-else></SpinnerComponent>
         </div>
       </div>
     </template>
