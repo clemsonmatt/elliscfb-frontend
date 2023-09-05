6ac4b406f64967dd04e9d1a4721e3b6e6eb5ed10
Matt Ellis <mellis0292@gmail.com>
2023-09-05 10:59:15 -0400

    Update main navbar for mobile improvements

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -8,65 +8,28 @@ const authStore = useAuthStore()
 
 <template>
   <div class="bg-base-200">
-    <div class="xl:mx-4 2xl:container 2xl:mx-auto">
-      <div class="navbar bg-base-200 xl:px-0">
-        <div class="navbar-start">
-          <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-5 h-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabindex="0"
-              class="p-2 mt-3 shadow-lg menu dropdown-content bg-base-200 rounded-box w-52"
+    <div class="pb-0 navbar">
+      <div class="flex-none">
+        <div class="dropdown lg:hidden">
+          <label tabindex="0" class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-5 h-5 stroke-current"
             >
-              <li>
-                <router-link
-                  :to="{ name: 'cfb_pickem' }"
-                  :class="{ active: currentRouteName == 'cfb_pickem' }"
-                >
-                  Pick'em
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  :to="{ name: 'cfb_teams' }"
-                  :class="{ active: currentRouteName == 'cfb_teams' }"
-                >
-                  Teams
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  :to="{ name: 'cfb_games' }"
-                  :class="{ active: currentRouteName == 'cfb_games' }"
-                >
-                  Games
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  :to="{ name: 'cfb_rankings' }"
-                  :class="{ active: currentRouteName == 'cfb_rankings' }"
-                >
-                  Rankings
-                </router-link>
-              </li>
-            </ul>
-          </div>
-          <ul class="hidden px-1 lg:flex menu menu-horizontal">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="p-2 mt-3 shadow-lg menu dropdown-content bg-base-200 rounded-box w-52"
+          >
             <li>
               <router-link
                 :to="{ name: 'cfb_pickem' }"
@@ -101,36 +64,71 @@ const authStore = useAuthStore()
             </li>
           </ul>
         </div>
-        <div class="navbar-end">
-          <div class="flex gap-2">
-            <div class="form-control" v-if="false">
-              <input type="text" placeholder="Search teams..." class="input input-bordered" />
-            </div>
-            <div class="dropdown dropdown-end">
-              <label tabindex="0" class="btn btn-circle">
-                <div class="w-10 rounded-full">{{ authStore.user.initials }}</div>
-              </label>
-              <ul
-                tabindex="0"
-                class="p-2 mt-3 shadow-lg menu dropdown-content bg-base-200 rounded-box w-52"
-              >
-                <li>
-                  <router-link :to="{ name: 'cfb_profile' }" class="justify-between">
-                    Profile
-                  </router-link>
-                </li>
-                <li>
-                  <router-link
-                    :to="{ name: 'cfb_settings_pickem' }"
-                    :class="{ active: $route.fullPath.includes('settings') }"
-                    v-if="authStore.canManage"
-                  >
-                    Settings
-                  </router-link>
-                </li>
-                <li><a @click="authStore.logout()">Logout</a></li>
-              </ul>
-            </div>
+        <ul class="hidden px-1 lg:flex menu menu-horizontal">
+          <li>
+            <router-link
+              :to="{ name: 'cfb_pickem' }"
+              :class="{ active: currentRouteName == 'cfb_pickem' }"
+            >
+              Pick'em
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'cfb_teams' }"
+              :class="{ active: currentRouteName == 'cfb_teams' }"
+            >
+              Teams
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'cfb_games' }"
+              :class="{ active: currentRouteName == 'cfb_games' }"
+            >
+              Games
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'cfb_rankings' }"
+              :class="{ active: currentRouteName == 'cfb_rankings' }"
+            >
+              Rankings
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="flex-1"></div>
+      <div class="flex-none">
+        <div class="flex gap-2">
+          <div class="form-control" v-if="false">
+            <input type="text" placeholder="Search teams..." class="input input-bordered" />
+          </div>
+          <div class="dropdown dropdown-end">
+            <label tabindex="0" class="btn btn-circle">
+              <div class="w-10 rounded-full">{{ authStore.user.initials }}</div>
+            </label>
+            <ul
+              tabindex="0"
+              class="p-2 mt-3 shadow-lg menu dropdown-content bg-base-200 rounded-box w-52"
+            >
+              <li>
+                <router-link :to="{ name: 'cfb_profile' }" class="justify-between">
+                  Profile
+                </router-link>
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'cfb_settings_pickem' }"
+                  :class="{ active: $route.fullPath.includes('settings') }"
+                  v-if="authStore.canManage"
+                >
+                  Settings
+                </router-link>
+              </li>
+              <li><a @click="authStore.logout()">Logout</a></li>
+            </ul>
           </div>
         </div>
       </div>
